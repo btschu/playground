230e751ef5de611813631ca604d27e503e2e6419
Brandon Schumacher <92617960+btschu@users.noreply.github.com>
2021-12-18 22:39:11 -0700

    starting point

--- server.py
+++ server.py
@@ -1,1 +1,27 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return 'Hello World!'
+
+@app.route('/dojo')
+def dojo():
+    return 'Dojo!'
+
+@app.route('/say/<string:name>')
+def say(name):
+    return f"Hi {name}"
+
+@app.route('/repeat/<int:num>/<string:name>')
+def repeat(num, name):
+    return f"{name * num}"
+
+@app.errorhandler(404)
+def pageNotFound(missing):
+    return "Sorry! No response. Try again."
+
+if __name__=="__main__":
+    app.run(debug=True)
 
