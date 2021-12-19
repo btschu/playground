5fab9df1b9a1dd9f97183887fc664a44582fb5c0
Brandon Schumacher <92617960+btschu@users.noreply.github.com>
2021-12-19 14:42:45 -0700

    Everything works with 1 template

--- server.py
+++ server.py
@@ -2,21 +2,17 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
-@app.route('/')
-def index():
-    return render_template("index.html")
-
 @app.route('/play')
 def play():
-    return render_template("play.html")
+    return render_template("index.html", boxNum = 3, color = "lightblue")
 
 @app.route('/play/<int:boxNum>')
 def play_num(boxNum):
-    return render_template('play.html', boxNum=boxNum)
+    return render_template('index.html', boxNum=boxNum, color = "lightblue")
 
 @app.route('/play/<int:boxNum>/<color>')
 def play_color(boxNum, color):
-    return render_template('play.html', color=color, boxNum=boxNum,)
+    return render_template('index.html', color=color, boxNum=boxNum,)
 
 @app.errorhandler(404)
 def pageNotFound(missing):
